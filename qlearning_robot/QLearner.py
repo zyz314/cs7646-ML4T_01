62d103fc201c55bf9f91261f9b61634f645a97ba
fyuen3 <fyuen3@gatech.edu>
2022-03-26 07:46:37 -0700

    qlearning

--- qlearning_robot/QLearner.py
+++ qlearning_robot/QLearner.py
@@ -22,9 +22,9 @@ GT honor code violation.
   		  	   		  	  			  		 			     			  	 
 -----do not edit anything above this line---  		  	   		  	  			  		 			     			  	 
   		  	   		  	  			  		 			     			  	 
-Student Name: Tucker Balch (replace with your name)  		  	   		  	  			  		 			     			  	 
-GT User ID: tb34 (replace with your User ID)  		  	   		  	  			  		 			     			  	 
-GT ID: 900897987 (replace with your GT ID)  		  	   		  	  			  		 			     			  	 
+Student Name: Fung Yi Yuen (replace with your name)  		  	   		  	  			  		 			     			  	 
+GT User ID: fyuen3 (replace with your User ID)  		  	   		  	  			  		 			     			  	 
+GT ID: 903641501 (replace with your GT ID)  	  	   		  	  			  		 			     			  	 
 """  		  	   		  	  			  		 			     			  	 
   		  	   		  	  			  		 			     			  	 
 import random as rand  		  	   		  	  			  		 			     			  	 
